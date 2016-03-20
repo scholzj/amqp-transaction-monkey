36472e0a05e7c747205cbf4874946e2914f1d21d
Jakub Scholz <www@scholzj.com>
2016-03-20 23:18:08 +0100

    More readable tests for ports

--- src/test/java/cz/scholz/amqp/transactionmonkey/tests/ConfigurationTest.java
+++ src/test/java/cz/scholz/amqp/transactionmonkey/tests/ConfigurationTest.java
@@ -53,135 +53,45 @@ public class ConfigurationTest {
     }
 
     @Test
-    public void testInvalidPort()
+    public void testPorts()
     {
+        Configuration config = new Configuration();
+
         try {
-            List<String> options = new ArrayList<>();
-            options.add("--first-broker-host");
-            options.add("host1");
-            options.add("--first-broker-port");
-            options.add("xxx");
-            options.add("-first-broker-username");
-            options.add("user1");
-            options.add("--first-broker-password");
-            options.add("password1");
-            options.add("--first-broker-queue");
-            options.add("queue1");
-            options.add("--second-broker-host");
-            options.add("host2");
-            options.add("--second-broker-port");
-            options.add("22222");
-            options.add("--second-broker-username");
-            options.add("user2");
-            options.add("--second-broker-password");
-            options.add("password2");
-            options.add("--second-broker-queue");
-            options.add("queue2");
+            config.setaPort("5672");
+            Assert.assertEquals("5672", config.getaPort());
+        }
+        catch (ConfigurationException e) {
+            Assert.fail("Valid port for first broker was not accepted - 5672");
+        }
 
-            String[] args = new String[options.size()];
-            options.toArray(args);
+        try {
+            config.setbPort("5672");
+            Assert.assertEquals("5672", config.getbPort());
+        }
+        catch (ConfigurationException e) {
+            Assert.fail("Valid port for second broker was not accepted - 5672");
+        }
 
-            Configuration config = new Configuration(args);
-            Assert.fail("Invalid port for first broker was not detected");
+        try {
+            config.setaPort("xxx");
+            Assert.fail("Invalid port for first broker was not detected - string");
         }
         catch (ConfigurationException e) {
             //pass
         }
 
         try {
-            List<String> options = new ArrayList<>();
-            options.add("--first-broker-host");
-            options.add("host1");
-            options.add("--first-broker-port");
-            options.add("11111");
-            options.add("-first-broker-username");
-            options.add("user1");
-            options.add("--first-broker-password");
-            options.add("password1");
-            options.add("--first-broker-queue");
-            options.add("queue1");
-            options.add("--second-broker-host");
-            options.add("host2");
-            options.add("--second-broker-port");
-            options.add("xxx");
-            options.add("--second-broker-username");
-            options.add("user2");
-            options.add("--second-broker-password");
-            options.add("password2");
-            options.add("--second-broker-queue");
-            options.add("queue2");
-
-            String[] args = new String[options.size()];
-            options.toArray(args);
-
-            Configuration config = new Configuration(args);
-            Assert.fail("Invalid port for second broker was not detected");
+            config.setaPort("66666");
+            Assert.fail("Invalid port for first broker was not detected - 66666");
         }
         catch (ConfigurationException e) {
             //pass
         }
 
         try {
-            List<String> options = new ArrayList<>();
-            options.add("--first-broker-host");
-            options.add("host1");
-            options.add("--first-broker-port");
-            options.add("0");
-            options.add("-first-broker-username");
-            options.add("user1");
-            options.add("--first-broker-password");
-            options.add("password1");
-            options.add("--first-broker-queue");
-            options.add("queue1");
-            options.add("--second-broker-host");
-            options.add("host2");
-            options.add("--second-broker-port");
-            options.add("22222");
-            options.add("--second-broker-username");
-            options.add("user2");
-            options.add("--second-broker-password");
-            options.add("password2");
-            options.add("--second-broker-queue");
-            options.add("queue2");
-
-            String[] args = new String[options.size()];
-            options.toArray(args);
-
-            Configuration config = new Configuration(args);
-            Assert.fail("Invalid port for first broker was not detected");
-        }
-        catch (ConfigurationException e) {
-            //pass
-        }
-
-        try {
-            List<String> options = new ArrayList<>();
-            options.add("--first-broker-host");
-            options.add("host1");
-            options.add("--first-broker-port");
-            options.add("66666");
-            options.add("-first-broker-username");
-            options.add("user1");
-            options.add("--first-broker-password");
-            options.add("password1");
-            options.add("--first-broker-queue");
-            options.add("queue1");
-            options.add("--second-broker-host");
-            options.add("host2");
-            options.add("--second-broker-port");
-            options.add("22222");
-            options.add("--second-broker-username");
-            options.add("user2");
-            options.add("--second-broker-password");
-            options.add("password2");
-            options.add("--second-broker-queue");
-            options.add("queue2");
-
-            String[] args = new String[options.size()];
-            options.toArray(args);
-
-            Configuration config = new Configuration(args);
-            Assert.fail("Invalid port for first broker was not detected");
+            config.setaPort("0");
+            Assert.fail("Invalid port for first broker was not detected - 66666");
         }
         catch (ConfigurationException e) {
             //pass
